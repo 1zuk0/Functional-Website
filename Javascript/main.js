d68f8f2f4236c739076d8de8f58ab0cb923bbfd7
1zuk0 <71511327+1zuk0@users.noreply.github.com>
2020-10-29 16:47:03 -0400

    Push #6 Javascript Update

---
+++ Javascript/main.js
@@ -0,0 +1,55 @@
+function assignmentOne() {
+  let name = prompt("Do you have proper indentification? State the verification code below")
+  alert("Code is incorrect. indentification invalid. Your access to Assignment 1 is denied.")
+}
+
+function assignmentTwo() {
+  alert ('The second assignment will be coming soon!')
+}
+
+
+function assignmentThree() {
+  alert ('The third assignment is on it/s way!')
+}
+
+
+function assignmentFour() {
+  alert ('The fourth assignment is also on it/s way!')
+}
+
+
+function assignmentFive() {
+ alert ('Wow seriously? Are you really still trying to view my assignments? Who are you my teacher??')
+}
+
+
+function assignmentSix() {
+ prompt ('Aight buddy, you have gone far enough. Let/s see some I.D.')
+ alert("That's some pretty illegal I.D. I don't think you should be here Pablo Dantonio")
+}
+
+
+function assignmentSeven() {
+  alert ('Sorry. The original function is taking a nap right now.')
+}
+
+
+function assignmentEight() {
+ prompt('Do you have the verification code?')
+ alert ("So you don't. I see..... You'd better scram ya punk")
+}
+
+
+function assignmentNine() {
+  alert ('Assignment nine took a nap')
+}
+
+
+function assignmentTen() {
+ alert ('Wow. You are creeping me out man. Do you not see the pattern? Pretty much all my assignments are coming soon. So be patient and wait... Yah moron.')
+}
+
+
+
+
+
